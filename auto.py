ebae1d6d4474073e27a74f551fb957fc3745a4a4
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-04-21 21:37:06 -0300

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,9 +1,13 @@
 import subprocess
 import time
+import os
+
+# Redireciona a saída padrão e de erro para /dev/null
+FNULL = open(os.devnull, 'w')
 
 while True:
-    # Executa o script vpn.py
-    subprocess.run(["python3", "vpn.py"])
+    # Executa o script vpn.py silenciosamente
+    subprocess.run(["python3", "vpn.py"], stdout=FNULL, stderr=subprocess.STDOUT)
 
     # Espera 5 segundos antes de rodar novamente
     time.sleep(5)
