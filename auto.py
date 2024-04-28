c35e3e3fff56316d1bab2fd625258ac8433f726d
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2024-04-28 16:12:20 +0000

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,37 +1,19 @@
-import requests
-import random
+import stem.process
+import stem.control
 import time
 
-proxies = [
-    {'http': 'http://103.109.117.13:8080'},
-    {'http': 'http://113.53.200.132:8080'},
-    {'http': 'http://116.203.15.220:8080'},
-    {'http': 'http://103.226.80.94:8080'},
-    {'http': 'http://117.55.124.114:8080'},
-    {'http': 'http://120.77.174.172:8080'},
-    {'http': 'http://182.61.58.12:8080'},
-    {'http': 'http://183.166.224.12:8080'},
-    {'http': 'http://192.168.1.105:8080'},
-    {'http': 'http://198.50.220.44:8080'},
-    {'http': 'http://103.109.117.13:8080'},
-    {'http': 'http://113.53.200.132:8080'},
-    {'http': 'http://116.203.15.220:8080'},
-    {'http': 'http://103.226.80.94:8080'},
-    {'http': 'http://117.55.124.114:8080'},
-    {'http': 'http://120.77.174.172:8080'},
-    {'http': 'http://182.61.58.12:8080'},
-    {'http': 'http://183.166.224.12:8080'},
-    {'http': 'http://192.168.1.105:8080'},
-    {'http': 'http://198.50.220.44:8080'}
-]
-
 def change_ip():
-    proxy = random.choice(proxies)
-    try:
-        r = requests.get('http://icanhazip.com', proxies=proxy, timeout=10)
-        print("Novo IP:", r.text.strip())
-    except Exception as e:
-        print("Erro ao mudar o IP:", e)
+    with stem.control.Controller.from_port(port=9051) as controller:
+        controller.authenticate()  # Autenticar com o controlador do Tor
+
+        # Pedir para o Tor obter uma nova identidade
+        controller.signal(stem.Signal.NEWNYM)
+
+        # Aguardar até que o novo IP esteja pronto
+        time.sleep(controller.get_newnym_wait())
+
+        # Obter o novo IP
+        print("Novo IP:", requests.get('http://icanhazip.com').text.strip())
 
 if __name__ == "__main__":
     try:
